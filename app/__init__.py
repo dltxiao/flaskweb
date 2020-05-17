21655da7e0faaf451467c1b3e17160555f19afaa
ZhongXiao <729457563@qq.com>
2020-05-18 00:59:24 +0800

    调整目录结构

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,13 @@ from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from .config import config
+from flask_migrate import Migrate
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -19,6 +21,7 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    migrate.init_app(app)
 
     # 添加路由和自定义的错误页面
     from .main import main as main_blueprint
